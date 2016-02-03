2ec3e132356cbe148e07e8f186e969db10d002dd
Kyle <kyle.ratti@outlook.com>
2016-02-03 13:26:59 -0500

    Added SKU scanning
    Very basic, still needs styling and error checking

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,30 @@
 var arrIMEI = {};
+var arrSKUs = {
+  "885909971343": "iPhone 6+ Space Gray 16GB",
+  "885909971367": "iPhone 6+ Gold 16GB",
+  "885909971350": "iPhone 6+ Silver 16GB",
+
+  "888462501583": "iPhone 6s+ Rose Gold 64GB",
+  "888462501576": "iPhone 6s+ Gold 64GB",
+
+  "888462501521": "iPhone 6s+ Silver 16GB",
+  "888462501514": "iPhone 6s+ Space Gray 16GB",
+
+  "888462500395": "iPhone 6s Gold 16GB",
+  "888462500388": "iPhone 6s Silver 16GB",
+  "888462500371": "iPhone 6s Space Gray 16GB",
+
+  "888462500418": "iPhone 6s Space Gray 64GB",
+  "888462500432": "iPhone 6s Gold 64GB"
+};
 var iErrorNum = 0;
 
-$(function() {
-  var objSKU = $("#deviceSKU");
-  var objIMEI = $("#deviceIMEI");
+var strInProgressSKU = null;
 
-  objIMEI.focus();
+$(function() {
+  var objDeviceInput = $("#deviceInput");
+
+  objDeviceInput.focus();
 });
 
 function addError(strError) {
@@ -15,32 +34,89 @@ function addError(strError) {
   iErrorNum++;
 }
 
-function processIMEIEntry() {
-  var objIMEI = $("#deviceIMEI");
-  var strText = objIMEI.val();
+function updateDetectedSKU() {
+  var objDetectedSKU = $("#detectedSKU");
+  var objDetectedDeviceName = $("#detectedDeviceName");
 
-  if(strText.length == 15) {
-    if(arrIMEI[strText] == true) {
-      addError("IMEI <b>" + strText + "</b> has already been added");
-    } else {
-      addDevice(strText);
-    }
-
-    objIMEI.val("");
-    objIMEI.focus();
+  if(strInProgressSKU == null || arrSKUs[strInProgressSKU] == null) {
+    objDetectedSKU.hide();
+  } else {
+    objDetectedDeviceName.text(arrSKUs[strInProgressSKU]);
+    objDetectedSKU.show();
   }
 }
 
-$("#deviceIMEI").on("input", processIMEIEntry);
-$("#generator").submit(processIMEIEntry);
+$("#detectedSKU").click(function() {
+  strInProgressSKU = null;
+  updateDetectedSKU();
+  $("#deviceInput").focus();
+});
 
-function addDevice(strIMEI) {
+function processDeviceInputEntry() {
+  var objDeviceInput = $("#deviceInput");
+  var strDeviceText = objDeviceInput.val();
+
+  if (strDeviceText.length == 12 && arrSKUs[strDeviceText] != null) {
+    console.log("detected sku " + strDeviceText);
+    strInProgressSKU = strDeviceText;
+    objDeviceInput.val("");
+  } else if(strDeviceText.length == 15) {
+    console.log("detected imei " + strDeviceText);
+    var objDeviceList = $("#deviceList");
+    objDeviceList.after('<div class="device" id="' + strDeviceText + '"></div>');
+
+    var objBarcode = $("#" + strDeviceText).barcode(strDeviceText, "code128", {
+      barHeight: 30
+    });
+
+    if(strInProgressSKU != null && strInProgressSKU != "") {
+      objBarcode.prepend('<span class="deviceName">' + arrSKUs[strInProgressSKU] + '</span>');
+    }
+
+    arrIMEI[strDeviceText] = true;
+    strInProgressSKU = null;
+    objDeviceInput.val("");
+  }
+
+  updateDetectedSKU();
+}
+
+$("#generator").submit(function(event) {
+  processDeviceInputEntry();
+  event.preventDefault();
+});
+
+function addDevice(strSKU, strIMEI) {
   var objDeviceList = $("#deviceList");
+  var strSKU = $("#deviceSKU").val();
+  var strDeviceName = arrSKUs[strSKU] || "";
   objDeviceList.after('<div class="device" id="' + strIMEI + '" href="#"></div>');
 
-  $("#" + strIMEI).barcode(strIMEI, "code128", {
+  var objBarcode = $("#" + strIMEI).barcode(strIMEI, "code128", {
     barHeight: 30
   });
 
+  if(strDeviceName != "") {
+    objBarcode.prepend('<span class="deviceName">' + strDeviceName + '</span>');
+  }
+
   arrIMEI[strIMEI] = true;
 }
+
+$("#inputMode").change(function(e) {
+  var objTarget = $(this);
+  var objIMEIEntry = $("#deviceIMEI");
+  var objSKUEntry = $("#deviceSKU");
+
+  if(objTarget.val() == "SKU & IMEI") {
+    $("#skuInput").show();
+    objIMEIEntry.val("");
+    objSKUEntry.val("");
+  } else {
+    $("skuInput").hide();
+    objIMEIEntry.val("");
+    objSKUEntry.val("");
+  }
+
+  setProperFocus();
+});
