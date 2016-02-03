db9fc968cef52267f403a3bacf538c66ced59af3
Kyle <kyle.ratti@outlook.com>
2016-02-03 10:20:00 -0500

    Fixed out of sync branches

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,19 @@
 var arrIMEI = {};
 var iErrorNum = 0;
 
-function addError(strError) {
-  $("#error-log").append('<li id="error' + iErrorNum + '">' + strError + '</li>')
-  $("#errors").removeClass("hidden");
-  $("#error" + iErrorNum).delay(5000).fadeOut('slow');
-  iErrorNum++;
-}
-
 $(function() {
   var objSKU = $("#deviceSKU");
   var objIMEI = $("#deviceIMEI");
 
   objIMEI.focus();
 });
+
+function addError(strError) {
+  $("#error-log").append('<li id="error' + iErrorNum + '">' + strError + '</li>')
+  $("#errors").removeClass("hidden");
+  $("#error" + iErrorNum).delay(5000).fadeOut('slow');
+  iErrorNum++;
+}
 
 function processIMEIEntry() {
   var objIMEI = $("#deviceIMEI");
@@ -35,14 +35,12 @@ $("#deviceIMEI").on("input", processIMEIEntry);
 $("#generator").submit(processIMEIEntry);
 
 function addDevice(strIMEI) {
-  var objTable = $("#deviceTable tr:last");
-  objTable.after('<tr><td id="' + strIMEI + '"></td> <td><a href="#" id="del' + strIMEI + '"><span class="glyphicon glyphicon-remove"></span></a></td></tr>');
+  var objDeviceList = $("#deviceList");
+  objDeviceList.after('<div class="device" id="' + strIMEI + '" href="#"></div>');
 
-  $("#" + strIMEI).barcode(strIMEI, "code128");
+  $("#" + strIMEI).barcode(strIMEI, "code128", {
+    barHeight: 30
+  });
 
   arrIMEI[strIMEI] = true;
-}
-
-function removeDevice(strIMEI) {
-  //
 }
