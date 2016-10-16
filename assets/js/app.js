194bb68abe25a7321af3a1c4f933f38c702f65e0
Kyle <kyleratti@users.noreply.github.com>
2016-10-15 21:01:50 -0400

    Orange > red

--- assets/js/app.js
+++ assets/js/app.js
@@ -147,7 +147,7 @@ $("#generator").submit(function(objEvent) {
 $(function() {
 	var objDeviceStatus = $("#deviceStatus");
 	var iNumDevices = inventory.getNumDevices();
-	objDeviceStatus.html('currently supporting <span class="label label-danger">' + iNumDevices + '</span> device' + (iNumDevices == 1 ? '' : 's')).show();
+	objDeviceStatus.html('currently supporting <span class="label label-warning">' + iNumDevices + '</span> device' + (iNumDevices == 1 ? '' : 's')).show();
 	app.rebuild();
 	setInputFocus();
 });
